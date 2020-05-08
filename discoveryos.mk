9bf38b243d484b83912fb5fc43876b94aff461a4
steadfasterX <steadfasterX@gmail.com>
2020-05-08 11:28:03 +0200

    gtexslte: initial bring-up
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

---
+++ discoveryos.mk
@@ -0,0 +1,41 @@
+#########################################################
+#
+# discoveryOS
+# Copyright: 2020 steadfasterX <steadfasterX@gmail.com>
+#
+#########################################################
+
+# add specific diOS overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/dios/overlay
+
+# discoverOS special handlings and settings
+# grab the overwrite vars from: build/tools/buildinfo.sh
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_DISPLAY_ID="discoveryOS-14.1.0" \
+	TARGET_BUILD_TYPE="user" \
+	TARGET_BUILD_FLAVOR="discoveryOS"
+
+# reduce ROM, increase speed
+PRODUCT_PACKAGES += ReduceROM
+
+# Set device ownership for the navigation app
+#PRODUCT_COPY_FILES += \
+#    device/samsung/gtexslte/device_owner.xml:data/system/device_owner.xml \
+#    device/samsung/gtexslte/device_policies.xml:data/system/device_policies.xml
+
+# enhance speech for navigation
+PRODUCT_COPY_FILES += \
+	vendor/dios/prebuilt/system/etc/codec_pga.xml:system/etc/codec_pga.xml
+
+# boot animation
+PRODUCT_COPY_FILES += \
+	vendor/dios/prebuilt/system/media/bootanimation.zip:system/media/bootanimation.zip
+
+# automate
+PRODUCT_COPY_FILES += \
+	vendor/dios/packages/automate.apk:system/priv-app/Automate/automate.apk
+
+# waze
+PRODUCT_COPY_FILES += \
+	vendor/dios/packages/waze.apk:system/app/Waze/waze.apk
+
---
+++ shrink/Android.mk
@@ -0,0 +1,66 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_JAVA_LIBRARIES := android-common
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_CERTIFICATE := shared
+LOCAL_PACKAGE_NAME := ReduceROM
+# It overrrides packages to avoid their addition to the ROM
+LOCAL_OVERRIDES_PACKAGES := \
+    Browser \
+    Calculator \
+    Calendar \
+    Camera2 \
+    CalendarProvider \
+    Email \
+    Gallery2 \
+    Music \
+    Eleven \
+    SoundRecorder \
+    Recorder \
+    VideoEditor \
+    Gallery \
+    AudioFX \
+    ExactCalculator \
+    Jelly \
+    LockClock \
+    DeskClock \
+    EasterEgg \
+    Updater \
+    WallpaperPicker \
+    WeatherProvider \
+    Exchange2 \
+    gdbserver \
+    powertop \
+    strace \
+    unrar \
+    rsync \
+    scp \
+    ssh \
+    sftp \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh \
+    Profiles \
+    WeatherManagerService \
+    libemoji \
+    LiveWallpapersPicker \
+    PhotoTable \
+    Terminal \
+    procmem \
+    procrank
+
+#	Launcher3 LatinIME Settings \
+#	Bluetooth \
+#	PicoTts \
+#	DeskClock Contacts Settings StorageManager TelephonyProvider \
+#	Trebuchet LineageSetupWizard \
+#	CMSettingsProvider \
+#
+#	Bluetooth BluetoothMidiService \
+#	CertInstaller messaging PicoTts \
+#	CtsShimPrebuilt DeskClock HTMLViewer UserDictionaryProvider webview CarrierConfig CellBroadcastReceiver CMAudioService CMSettingsProvider Contacts ContactsProvider LineageSetupWizar Settings MediaProvider Shell StorageManager Telecom TelephonyProvider TeleService Dialer Trebuchet
+
+include $(BUILD_PACKAGE)
